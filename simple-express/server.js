1df58a11c2584c771a80f7609b8a4e955407fc63
fang <u2mtv25@gmail.com>
2021-06-18 16:16:51 +0800

    add pug pages

--- simple-express/server.js
+++ simple-express/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//載入靜態檔案
+app.use(express.static("public"));
+
+// 第一個是變數 views
+// 第二個是檔案夾名稱
+app.set("views", "views");
+// 告訴 express 我們用的 view engine 是 pug
+app.set("view engine", "pug");
+
 app.use(function (req, res, next) {
   let current = new Date();
   console.log(`有人來訪問了喔 在 ${current}`);
@@ -9,11 +18,13 @@ app.use(function (req, res, next) {
   next();
 });
 app.get("/", (req, res) => {
-  res.send("Hello express!");
+  // res.send("Hello express!");
+  res.render("index");
 });
 
 app.get("/about", function (req, res) {
-  res.send("About Express AAAAAA");
+  // res.send("About Express");
+  res.render("about");
 });
 
 app.get("/test", function (req, res) {
