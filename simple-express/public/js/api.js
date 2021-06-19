b66f479f5b43de83de1f4708ffa210ebd1f99398
fang <u2mtv25@gmail.com>
2021-06-19 17:21:48 +0800

    完成使用三種方式取得API

--- simple-express/public/js/api.js
+++ simple-express/public/js/api.js
@@ -1,12 +1,31 @@
+//AJAX
 $.ajax({
   method: "GET",
   url: "/api/stocks",
-//   dataType: "json",
+  dataType: "json",
 })
   .done(function (data) {
+    console.log("AJAX");
     console.log(data);
   })
   .fail(function () {
     alert("error");
   })
   .always(function () {});
+
+//AXIOS
+
+(async function () {
+  let get = await axios.get("/api/stocks");
+    console.log("Axios");
+    console.log(get.data);
+})();
+
+//FETCH
+
+(async function () {
+  let get = await fetch("/api/stocks");
+  let json = await get.json();
+  console.log("fetch");
+  console.log(json);
+})();
